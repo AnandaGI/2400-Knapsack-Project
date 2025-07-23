aa56ca7d64825943d7ab4fd53f878e8a00a35f3b
AnandaGI <agirwin42@tntech.edu>
2025-07-23 12:56:52 -0500

    Update input_gen to allow for generation of items to output file
     - New random generation of structures and output to file is Adam Light's work
    
     - Updated array to a vector for simplification

--- input_generation.cpp
+++ input_generation.cpp
@@ -1,13 +1,19 @@
 /*
+Authors: Ananda Irwin, Adam Light
+Created: 7/22/2025
+Modified 7/23/2025
 This file contains the item structure that will be stored in the input arrays.
 It additionally contains the algorithm for generating these input array.
 */
+
 #ifndef INPUT_GEN_H
 #define INPUT_GEN_H
 
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <vector>
+#include <random>
 
 
 struct item{
@@ -15,46 +21,71 @@ struct item{
     uint8_t value;
 };
 
-//Implement algorithm to create input arrays
-item** retrieve_arr(std::string filepath, int size){
+std::vector<item> retrieve_arr(std::string filepath, int size){
     std::ifstream dataFile(filepath);
     std::string output;
-    item* *item_arr = new item*;
+    std::vector<item> item_arr;
     int i = 0;
     int delimPos;
-    int weight;
-    int val;
+    uint8_t weight;
+    uint8_t val;
 
     while (std::getline(dataFile, output) && i < size){
         delimPos = output.find(' ');
         weight = stoi(output.substr(0, delimPos));
         val = stoi(output.substr(delimPos+1));
+        item tempItem{weight, val};
 
-        item_arr[i] = new item;
-        item_arr[i]->weight = weight;
-        item_arr[i]->value = val;
+        item_arr.push_back(tempItem);
         i++;
     }
     return item_arr;
 };
 
+void create_data(std::string output_dir, int size){
+    std::vector<item> structArray(size);
+
+    std::mt19937 randomEngine(std::random_device{}());
+    std::uniform_int_distribution<unsigned short> dist(1, 255); // Must be 0-255 for uint8_t, at least 1 for item/value
+
+    // Populate structArray with random data
+    for (size_t i = 0; i < size; ++i) {
+        structArray[i].weight = static_cast<uint8_t>(dist(randomEngine));
+        structArray[i].value = static_cast<uint8_t>(dist(randomEngine));
+    }
+
+    // Write structArray to file
+    std::ofstream output_file(output_dir);
+    if (output_file.is_open()) {
+        for (size_t i = 0; i < size; ++i) {
+            output_file << static_cast<int>(structArray[i].weight) << " "
+                << static_cast<int>(structArray[i].value) << "\n";
+        }
+        output_file.close();
+        std::cout << "Struct data written to output file successfully.\n";
+    }
+    else {
+        std::cerr << "Error: Unable to open file for writing.\n";
+    }
+}
+
 void test_allocation(int size, std::string path){
-    item** item_arr = retrieve_arr(path, size);
+    create_data(path, size);
+    std::vector<item> item_arr = retrieve_arr(path, size);
 
     for(int i = 0; i < size; i++){
-        std::cout << std::to_string(item_arr[i]->weight) + " " + std::to_string(item_arr[i]->value) + "\n";
-        delete item_arr[i];
+        std::cout << std::to_string(item_arr[i].weight) + " " + std::to_string(item_arr[i].value) + "\n";
     }
-    delete [] item_arr;
 }
 
 int main(){
     int size;
     std::string path;
+    std::string file_name;
 
     std::cout << "What size array would you like to test for? ";
     std::cin >> size;
-    std::cout << "What is the file path? ";
+    std::cout << "What is the file path you would like to write to? ";
     std::cin >> path;
 
     test_allocation(size, path);
